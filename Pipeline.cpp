a93041774b3b8ce6c2a81a63b0ca40c31490a9c9
Antti Ala-Ilkka <antti@instereo.fi>
2024-01-17 16:24:13 +0200

    reconfigure before playing

--- Pipeline.cpp
+++ Pipeline.cpp
@@ -293,12 +293,15 @@ NAN_METHOD(Pipeline::PlayElement) {
 		g_print("Element to play not found\n");
 		return;
 	}
-	gst_element_set_state (e, GST_STATE_PLAYING);
-	g_print("Played element\n");
 
+	// Reconfigure
 	GstPad *srcpad = gst_element_get_static_pad(e, "src");
 	gst_pad_push_event(srcpad, gst_event_new_reconfigure());
 	g_print("Sent reconfigure event\n");
+
+	// Play
+	gst_element_set_state (e, GST_STATE_PLAYING);
+	g_print("Played element\n");
 }
 
 NAN_METHOD(Pipeline::StopElement) {
