85c2078b392048e692da8a494ea29e52bc953a04
= <=>
2025-06-01 22:35:01 +0000

    Rolled back context aware so it works in bun

--- gstreamer.cpp
+++ gstreamer.cpp
@@ -5,13 +5,18 @@
 #include "GObjectWrap.h"
 #include "Pipeline.h"
 
-
-extern "C" NODE_MODULE_EXPORT void
-NODE_MODULE_INITIALIZER(Local<Object> exports,
-                        Local<Value> module,
-                        Local<Context> context) {
-
+void init(Nan::ADDON_REGISTER_FUNCTION_ARGS_TYPE exports)
+{
 	gst_init(NULL, NULL);
 	GObjectWrap::Init();
 	Pipeline::Init(exports);
+}
+
+// NODE_MODULE(gstreamer_superficial, init);
+
+extern "C"
+{
+	static node::node_module _module = {127, 0, __null, "/home/sygnal/node-gstreamer-superficial/gstreamer.cpp", (node::addon_register_func)(init), __null, "gstreamer_superficial", __null, __null};
+	static void _register_gstreamer_superficial(void) __attribute__((constructor));
+	static void _register_gstreamer_superficial(void) { node_module_register(&_module); }
 }
